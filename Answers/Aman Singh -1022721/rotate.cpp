e2bb5db5e19cad9e9df537b6d7612e5d347d9fc2
Aman <amanthapliyal14@gmail.com>
2021-06-15 11:17:55 +0530

    answer uploading

--- Answers/Aman Singh -1022721/rotate.cpp
+++ Answers/Aman Singh -1022721/rotate.cpp
@@ -1,32 +1,31 @@
 // Write a program to rotate an object by 90 degree in clockwise direction
 
-#include<stdio.h>  
-#include<graphics.h>  
-#include<math.h>  
-int main()  
-{  
-    int gd=0,gm,x1,y1,x2,y2;  
-    double s,c, angle;  
-    initgraph(&gd, &gm, "");  
-    setcolor(RED);  
-    printf("Enter coordinates of line: ");  
-    scanf("%d%d%d%d",&x1,&y1,&x2,&y2);  
-    cleardevice();  
-    setbkcolor(WHITE);  
-    line(x1,y1,x2,y2);  
-    getch();  
-    setbkcolor(BLACK);  
-    printf("Enter rotation angle: ");  
-    scanf("%lf", &angle);  
-    c = cos(angle *3.14/180);  
-    s = sin(angle *3.14/180);  
-    x1 = floor(x1 * c + y1 * s);  
-    y1 = floor(-x1 * s + y1 * c);  
-    x2 = floor(x2 * c + y2 * s);  
-    y2 = floor(-x2 * s + y2 * c);  
-    cleardevice();  
-    line(x1, y1 ,x2, y2);  
-    getch();  
-    closegraph();  
-return 0;  
-}  
+
+#include<graphics.h>
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int gd=DETECT,gm;
+int pivot_x,pivot_y,x,y;
+double degree,radian;
+int rotated_point_x,rotated_point_y;
+initgraph(&gd,&gm,"C://TURBOC3//BGI");
+cleardevice();
+printf("\t\t***** ROTATION ***** \n");
+printf("\n Enter an initial coordinates of the line = ");
+scanf("%d %d",&pivot_x,&pivot_y);
+printf("\n Enter a final coordinates of the line = ");
+scanf("%d %d",&x,&y);
+line(pivot_x,pivot_y,x,y);
+printf("\n\n Now, Enter a degree = ");
+scanf("%lf",&degree);
+radian=degree*0.01745;
+rotated_point_x=(int)(pivot_x +((x-pivot_x)*cos(radian)-(y-pivot_y)*sin(radian)));
+rotated_point_y=(int)(pivot_y +((x-pivot_x)*sin(radian)+(y-pivot_y)*cos(radian)));
+setcolor(RED);
+line(pivot_x,pivot_y,rotated_point_x,rotated_point_y);
+getch();
+closegraph();
+}
